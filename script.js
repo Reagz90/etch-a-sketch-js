b15369829e9c464a33de0538af9dae6f3fe89eb7
Reagan Hammond <reagan.hammond@open.edu.au>
2022-09-13 14:38:46 +1000

    added dynamic div creation with styling and event listener on mouse hover to change background colour

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let sizeOfGrid = 5;
 
+const container = document.querySelector('.container'); 
+
+const createGrid = (amtofGrids) => {
+    for (let i = 0; i < amtofGrids; i++) {
+        const row = document.createElement('div')
+        row.classList.add('grid-row')
+
+        for (let j = 0; j < amtofGrids; j++) {
+            const widthAndHeight = 960 / sizeOfGrid
+            const gridBox = document.createElement('div')
+            gridBox.classList.add('grid-box')
+            gridBox.style.width = `${widthAndHeight}px`
+            gridBox.style.height = `${widthAndHeight}px`
+
+            gridBox.addEventListener('mouseenter', () => {
+                gridBox.style.backgroundColor = 'black'
+            })
+            row.appendChild(gridBox)
+        }
+
+        container.appendChild(row)
+    }
+}
+
+createGrid(sizeOfGrid); 
